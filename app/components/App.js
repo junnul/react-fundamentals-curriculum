79ca575cfe97e48d1f7a475b48b48c70ba3bd622
Junnu Lappalainen <junnu@polttoaine.net>
2017-09-28 10:36:53 +0300

    Add router

--- app/components/App.js
+++ app/components/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Header'
 import Home from './Home'
 
 export default class App extends Component {
     render() {
         return (
+        <Router>
             <div className="h-100">
                 <Header />
                 <div className="container-fluid m-0 p-0 d-flex w-100 h-100 justify-content-center align-items-center flex-column">
-                    <Home />
+                    <Route exact path="/" component={Home} />
                 </div>
             </div>
+        </Router>
         )
     }
 }
